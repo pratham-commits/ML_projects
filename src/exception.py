bba31d1e1a3d78754cb643de1e96dd246f27f2ca
pratham-commits <dumppp.00001@gmail.com>
2025-03-30 20:30:27 +0530

    Logging and Exception

---
+++ src/exception.py
@@ -0,0 +1,34 @@
+import sys
+import logging
+
+#any exception is being generated sys will be able to handle it
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    line_number=exc_tb.tb_lineno
+    error_msg=str(error)
+
+    #exc_tb will contain all the error info where the error has occurred , on which line error has occurred
+    error_message="Error occurred in python script [{0}] \n Line number [{1}] \n error message [{2}]".format(
+        file_name,
+        line_number,
+        error_msg
+    )
+    return error_message
+
+#this function needs to be called when an exception is to be raised
+#so for that we are creating a class below
+
+class CustomException(Exception):
+    def __init__(self, error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
+
+
+        
+
---
+++ src/logger.py
@@ -0,0 +1,20 @@
+import logging
+import os
+from datetime import datetime
+
+# we log every execution that occurs so that we are able to 
+# keep of very exception and execution happening
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+log_paths=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(log_paths,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(log_paths,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="%(asctime)s - %(levelname)s - %(filename)s - Line %(lineno)d - %(message)s",
+    level=logging.INFO,
+    filemode='a'
+)
+
