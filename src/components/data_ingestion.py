0a520ecae8d5130206c538c28d62f870d5769d8b
pratham-commits <dumppp.00001@gmail.com>
2025-04-02 00:53:06 +0530

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,9 +3,11 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
-
+from data_transformation import DataTransformation
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+#reading of dataset from various sources
 
 """
 The @dataclass decorator is used to simplify the 
@@ -63,6 +65,9 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
 
